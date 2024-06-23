1dd64c033cf1be607ae1dda9d38418213d9af29a
GeeksforGeeks <93614882+zeekouille@users.noreply.github.com>
2024-06-23 21:31:49 +0200

    Added price

--- client/src/components/Shaper.tsx
+++ client/src/components/Shaper.tsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import fragmentOfChimera from '../image/FragmentChimera.png'
+import fragmentOfPhoenix from '../image/FragmentPhoenix.png'
+import fragmentOfMinotaur from '../image/FragmentMinotaur.png'
+import fragmentOfHydra from '../image/FragmentHydra.png'
 
 export default function Shaper() {
-  const [costPerRun, setCostPerRun] = useState<number>(0);
+
   const [timePerRun, setTimePerRun] = useState<number>(0);
   const [fragmentOfKnowledge, setFragmentOfKnowledge] = useState<number>(303);
   const [fragmentOfShape, setFragmentOfShape] = useState<number>(350);
   const [starforge, setStarforge] = useState<number>(350);
   const [profit, setProfit] = useState<number | null>(null);
+
+  const [costPerRun, setCostPerRun] = useState<number>(0);
+  const [fragmentOfHydraPrice , setFragmentOfHydraPrice] = useState<number>(0);
+  const [fragmentOfPhoenixPrice, setFragmentOfPhoenixPrice] = useState<number>(0);
+  const [fragmentOfChimeraPrice, setFragmentOfChimeraPrice] = useState<number>(0);
+  const [fragmentOfMinotaurPrice, setFragmentOfMinotaurPrice] = useState<number>(0);
 
   const profitPerShaper = () =>
     (fragmentOfKnowledge + fragmentOfShape) / 4 + starforge * 0.035;
@@ -16,16 +26,52 @@ export default function Shaper() {
     setProfit(calculatedProfit);
   };
 
+  useEffect(() => {
+    setCostPerRun(fragmentOfHydraPrice + fragmentOfPhoenixPrice + fragmentOfChimeraPrice + fragmentOfMinotaurPrice);
+  } , [fragmentOfHydraPrice , fragmentOfChimeraPrice , fragmentOfMinotaurPrice , fragmentOfPhoenixPrice]);
+
   return (
     <>
       <div>Shaper</div>
 
-      <p>Cost per run</p>
+      <p>Cost per run :</p>
+      <img src={fragmentOfHydra} />
       <input
-        type="number"
-        value={costPerRun}
-        onChange={(e) => setCostPerRun(Number(e.target.value))}
-      />
+      type="number"
+      value={fragmentOfHydraPrice}
+      onChange={(e) => setFragmentOfHydraPrice(Number(e.target.value))}>
+      </input>
+      <br></br>
+     
+      <img src={fragmentOfPhoenix} />
+      <input
+      type="number"
+      value={fragmentOfPhoenixPrice}
+      onChange={(e) => setFragmentOfPhoenixPrice(Number(e.target.value))}>
+      </input>
+      <br></br>
+
+      <img src={fragmentOfChimera} />
+      <input
+      type="number"
+      value={fragmentOfChimeraPrice}
+      onChange={(e) => setFragmentOfChimeraPrice(Number(e.target.value))}>
+      </input>
+      <br></br>
+
+      <img src={fragmentOfMinotaur} />
+      <input
+      type="number"
+      value={fragmentOfMinotaurPrice}
+      onChange={(e) => setFragmentOfMinotaurPrice(Number(e.target.value))}>
+      </input>
+      <br></br>
+<p>Total cost per run : </p>
+      {costPerRun}
+    
+      
+      
+    
 
       <p>Time per run in sec</p>
       <input
@@ -34,9 +80,9 @@ export default function Shaper() {
         onChange={(e) => setTimePerRun(Number(e.target.value))}
       />
 
-      <button onClick={handleCalculateProfit}>Calculate Profit</button>
+      <button onClick={handleCalculateProfit}>Calculate Profit</button>;
 
-      <p>Profit per shaper</p>
+      <p>Profit per shaper</p>;
       <p>{profit !== null ? profit : 'Click the button to calculate profit'}</p>
     </>
   );
