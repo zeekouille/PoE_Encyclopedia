dccd1741617f8c90e7c4ba844d4dd77a8af0ca6c
GeeksforGeeks <93614882+zeekouille@users.noreply.github.com>
2024-06-29 20:25:17 +0200

    Added things for chaos bow crafting

--- client/src/components/ChaosDotBow.tsx
+++ client/src/components/ChaosDotBow.tsx
@@ -14,7 +14,17 @@ const ChaosDotBow: React.FC = () => {
         {currentStep === 1 && (
           <div>
             <h2>Étape 1</h2>
-            <p>Get Maraketh Bow</p>
+            <p>What will you need ?</p>
+            <a href="https://www.pathofexile.com/trade/search/Necropolis/EZDJdRzH5w" rel="noreferrer" target="_blank" className="sub-link">6l Maraketh Bow iLVL82</a>
+            <a href="https://www.pathofexile.com/trade/exchange/Necropolis/9z28fK" rel="noreferrer" target="_blank" className="sub-link">5 Divine orbs</a>
+            <a href="https://www.pathofexile.com/trade/exchange/Necropolis/j94oKzoIX" rel="noreferrer" target="_blank" className="sub-link">1 Hunter's exalted orb</a>
+            <a href="https://www.pathofexile.com/trade/exchange/Necropolis/rbdMHQ" rel="noreferrer" target="_blank" className="sub-link">1 Orb of Scouring</a>
+            <a href="https://www.pathofexile.com/trade/exchange/Necropolis/20vqoMock" rel="noreferrer" target="_blank" className="sub-link">5000 Vivid Force</a>
+            <a href="https://www.pathofexile.com/trade/exchange/Necropolis/YOWQWXvfY" rel="noreferrer" target="_blank" className="sub-link">Orb of Alterations</a>
+            <a href="https://www.pathofexile.com/trade/exchange/Necropolis/ZBjaG3RHQ" rel="noreferrer" target="_blank" className="sub-link">Regal Orb</a>
+            <a href="https://www.pathofexile.com/trade/exchange/Necropolis/vkoaMJLfE" rel="noreferrer" target="_blank" className="sub-link">Scouring Orb</a>
+            <a href="https://www.pathofexile.com/trade/exchange/Necropolis/jedLr7dTX" rel="noreferrer" target="_blank" className="sub-link">Orb Of Annulment</a>
+
             <div className="button-container">
               <button onClick={nextStep}>Suivant</button>
             </div>
@@ -23,17 +33,27 @@ const ChaosDotBow: React.FC = () => {
         {currentStep === 2 && (
           <div>
             <h2>Étape 2</h2>
-            <p>Disposez les bâtons en forme de triangle ouvert et les ficelles en zigzag.</p>
+            <p>Spam alterations + augment for T1 Damage Over Time Multiplier : Avg 257 alt / 55 augments</p>
+           
             <div className="button-container">
               <button onClick={previousStep}>Précédent</button>
               <button onClick={nextStep}>Suivant</button>
             </div>
           </div>
         )}
+        
         {currentStep === 3 && (
           <div>
             <h2>Étape 3</h2>
-            <p>Attachez les ficelles aux extrémités des bâtons pour former l'arc.</p>
+            <p>Regal</p>
+            <p>Annul until T1 DoT is isolated </p>
+            <p>if you brick t1 dot, back to alt spam</p>
+            <p>Multimod + cannot roll attack mods</p>
+            <p>Exalt slam for +1 socketed gems</p>
+            <p>Hunter slam for chaos dot guarenteed</p>
+            <p>remove multimod</p>
+            <p>Craft +2 to Level of Socketed Support Gems</p>
+            <p>Craft Chaos damage over time multiplier</p>
             <div className="button-container">
               <button onClick={previousStep}>Précédent</button>
               <button onClick={nextStep}>Suivant</button>
