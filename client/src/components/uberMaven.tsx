55d96a575989d1334bfc09767872a706706e3bfb
zeekouille <93614882+zeekouille@users.noreply.github.com>
2024-07-07 14:33:39 +0200

    Added table to ubermaven and possibility to enter custom prices to calculate profit

--- client/src/components/uberMaven.tsx
+++ client/src/components/uberMaven.tsx
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Layout from "./Layout";
 import { useData } from "./dataContext";
+import './bossProfitStyles.css'; // Assurez-vous d'importer votre fichier CSS
 
-// Définir un type pour les URLs des icônes
 interface ItemIcons {
   realityFragment: string;
   viridisVeil: string;
   progenesis: string;
   curioOfPotential: string;
   shinyReliquaryKey: string;
-  orbOfConflict: string,
-
+  orbOfConflict: string;
 }
 
-export const UberMaven = () => {
+const UberMaven = () => {
   const { data } = useData();
-  
-  // Utiliser un nom différent pour les URLs des icônes
+
   const [itemIcons, setItemIcons] = useState<ItemIcons>({
     realityFragment: "",
     viridisVeil: "",
@@ -24,11 +22,10 @@ export const UberMaven = () => {
     curioOfPotential: "",
     shinyReliquaryKey: "",
     orbOfConflict: "",
- 
   });
 
   const [itemPrices, setItemPrices] = useState<{
-    [itemName: string]: number | 0;
+    [itemName: string]: number;
   }>({
     realityFragment: 0,
     viridisVeil: 0,
@@ -36,7 +33,6 @@ export const UberMaven = () => {
     curioOfPotential: 0,
     shinyReliquaryKey: 0,
     orbOfConflict: 0,
-
   });
 
   useEffect(() => {
@@ -47,17 +43,16 @@ export const UberMaven = () => {
       { itemName: "Curio of Potential", stateKey: "curioOfPotential" },
       { itemName: "Shiny Reliquary Key", stateKey: "shinyReliquaryKey" },
       { itemName: "Orb of Conflict", stateKey: "orbOfConflict" },
-    
     ];
 
     const newPrices: { [key: string]: number } = {};
-    const newIcons: Partial<ItemIcons> = { ...itemIcons }; // Copie de l'état actuel des icônes
+    const newIcons: Partial<ItemIcons> = { ...itemIcons };
 
     itemsToFetch.forEach((item) => {
       const itemData = data.find((d) => d.name === item.itemName);
       if (itemData) {
         newPrices[item.stateKey] = itemData.mean;
-        newIcons[item.stateKey as keyof ItemIcons] = itemData.icon; // Utilisez keyof ItemIcons pour accéder aux clés correctement
+        newIcons[item.stateKey as keyof ItemIcons] = itemData.icon;
       }
     });
 
@@ -66,7 +61,7 @@ export const UberMaven = () => {
       ...newPrices,
     }));
 
-    setItemIcons(newIcons as ItemIcons); // Mettre à jour l'état des icônes
+    setItemIcons(newIcons as ItemIcons);
   }, [data]);
 
   const [profitPerBoss, setProfitPerBoss] = useState<number>(0);
@@ -77,9 +72,7 @@ export const UberMaven = () => {
       itemPrices.viridisVeil * 0.55 +
       itemPrices.progenesis * 0.11 +
       itemPrices.orbOfConflict * 0.35 +
-      itemPrices.curioOfPotential * 0.05 ;
-
-
+      itemPrices.curioOfPotential * 0.05;
 
     const profitPerBoss = rewardPerRun - pricePerRun;
     console.log("Profit per boss:", profitPerBoss);
@@ -87,23 +80,63 @@ export const UberMaven = () => {
     setProfitPerBoss(profitPerBoss);
   };
 
+  const handlePriceChange = (itemName: string, value: string) => {
+    const parsedValue = parseFloat(value);
+    if (!isNaN(parsedValue)) {
+      setItemPrices((prevPrices) => ({
+        ...prevPrices,
+        [itemName]: parsedValue,
+      }));
+    }
+  };
+
   return (
     <Layout>
-      <div>Maven</div>
-      {Object.keys(itemPrices).map((key) => (
-        <div key={key}>
-          <p>{key}</p>
-          <p>{itemPrices[key]}</p>
-          {itemIcons[key as keyof ItemIcons] && (
-            <img src={itemIcons[key as keyof ItemIcons]} alt={`${key} icon`} />
-          )}
-        </div>
-      ))}
+      <div className="centered-text">Uber Maven</div>
+      <table className="centered-table">
+        <thead>
+          <tr>
+            <th>Item Name</th>
+            <th>Icon</th>
+            <th>Price</th>
+            <th>Set Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(itemPrices).map((key) => (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>
+                {itemIcons[key as keyof ItemIcons] && (
+                  <img
+                    src={itemIcons[key as keyof ItemIcons]}
+                    alt={`${key} icon`}
+                    style={{ width: "50px", height: "50px" }}
+                  />
+                )}
+              </td>
+              <td>{itemPrices[key]}</td>
+              <td>
+                <input
+                  type="number"
+                  value={itemPrices[key]}
+                  onChange={(e) => handlePriceChange(key, e.target.value)}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-      <button onClick={calculateProfitPerBoss}>Calculate profit</button>
+      <button
+        style={{ textAlign: "center", display: "block", margin: "0 auto" }}
+        onClick={calculateProfitPerBoss}
+      >
+        Calculate profit
+      </button>
 
-      <p>Reward per boss</p>
-      <p>{profitPerBoss}</p>
+      <p className="centered-text">Reward per boss</p>
+      <p className="centered-text">{profitPerBoss}</p>
     </Layout>
   );
 };
